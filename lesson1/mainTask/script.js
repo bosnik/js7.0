4789474afae5205b78f33de78ce395d21b22c5c0
Rodion <rodion1991313@gmail.com>
2019-04-21 11:07:06 +0100

    отредактировал lesson1 - main

--- lesson1/mainTask/script.js
+++ lesson1/mainTask/script.js
@@ -1,8 +1,12 @@
 'use strict'
-let money = +prompt("Ващ бюджет на месяц?"),
+let money = prompt("Ващ бюджет на месяц?"),
 time = prompt("Введите дату в форме YYYY-MM-DD"),
-saving = false,
-oneDay = 30;
+question = prompt('Введите обязательную статью расходов в этом месяце'),
+answer = prompt('Во сколько обойдется?'),
+question1 = prompt('Введите обязательную статью расходов в этом месяце'),
+answer1 = prompt('Во сколько обойдется?'),
+saving = false;
+
 
 let appData = {
     timeData: time,
@@ -13,70 +17,16 @@ let appData = {
      optionalExpenses:{}
 };
 
+appData.expenses[question] = answer;
+appData.expenses[question1] = answer1;
+
+alert(money/30);
 console.log(appData);
 
-  for (let i = 0; i < 2; i++){
-          let question = prompt('Введите обязательную статью расходов в этом месяце'),
-          answer1 = prompt('Во сколько обойдется?');
+  
 
-          if ( (typeof(question))=== 'string' && (typeof(question)) != null 
-          && (typeof(answer1)) != null && question != ''
-          && answer1 != '' && question.length < 50 ){
-          console.log("Done");
-          appData.expenses[question] = answer1;
-         
-     }else{
-          
-     }
-};
 
-/* -------while-------- */
 
-/*  let question = prompt('Введите обязательную статью расходов в этом месяце'),
-     answer1 = prompt('Во сколько обойдется?');
 
-appData.expenses[question] = answer1;
-
-while((typeof(question))=== 'string' && (typeof(question)) != null 
-&& (typeof(answer1)) != null && question != ''
-&& answer1 != '' && question.length < 50 ){
-     console.log("Good");
-}
-appData.moneyPerDay = appData.budget / 30;
-alert("Ежедневный бюджет: " + appData.moneyPerDay); */
-
-/* -------while-------- */
-/* -------doWhile-------- */
-
-/* do{
-     let question = prompt('Введите обязательную статью расходов в этом месяце'),
-     answer1 = prompt('Во сколько обойдется?');
-     appData.expenses[question] = answer1;
-     appData.moneyPerDay = appData.budget / 30;
-     alert("Ежедневный бюджет: " + appData.moneyPerDay); 
-     console.log("Good");
-}while((typeof(question))=== 'string' && (typeof(question)) != null 
-&& (typeof(answer1)) != null && question != ''
-&& answer1 != '' && question.length < 50 ); */
-
-/* -------doWhile-------- */
-
-appData.moneyPerDay = appData.budget / 30;
-
-alert("Ежедневный бюджет: " + appData.moneyPerDay);
-
-if(appData.moneyPerDay < 100){
-     console.log("Минимальный уровень достанка");
-
-}else if(appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){
-     console.log("Средний уровень достанка");
-
-}else if(appData.moneyPerDay > 100){
-     console.log("Высокий уровень достанка");
-
-}else{
-     console.log("Произошла ошибка");
-
-}
 
 
