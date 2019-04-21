601f83fd3572eff53c1d62519546c7f64dc97718
Rodion <rodion1991313@gmail.com>
2019-04-21 15:20:47 +0100

    исправил 2,0 на 20

--- lesson1/additionalTask/script1.js
+++ lesson1/additionalTask/script1.js
@@ -1,10 +1,10 @@
 var num = 33721;
-var test = eval(num.toString().split('').join('*'));
-var step = test**3;
-var last = (eval(step.toString().split('')));
+var multiplication= eval(num.toString().split('').join('*'));
+var power = multiplication**3;
+var twoLast = eval(power.toString().slice(0,2));
 
-console.log(test);
-console.log(step);
-alert(last.slice(0,2));
+console.log(multiplication);
+console.log(power);
+alert(twoLast);
 
 
