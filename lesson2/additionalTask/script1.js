1ee263dda1377a728d2c4f15a500b3ca5f975527
Rodion <rodion1991313@gmail.com>
2019-04-21 23:40:01 +0100

    урок2 доп задания

--- lesson2/additionalTask/script1.js
+++ lesson2/additionalTask/script1.js
@@ -1,10 +1,31 @@
-var num = 33721;
-var test = eval(num.toString().split('').join('*'));
-var step = test**3;
-var last = (eval(step.toString().split('')));
 
-console.log(test);
-console.log(step);
-alert(last.slice(0,2));
+var week = 
+['Понедельник',
+'Вторник',
+'Среда',
+'Четверг',
+'Пятница',
+'Суббота',
+'Воскресение'];
+
+for (let i = 0; i < week.length; i++) {
+         if (week[i] == 'Суббота' || week[i] == 'Воскресение') {
+         document.write(`<p><b>${week[i]}</i></p>`);
+        } else if (week[i] == 'Среда') {
+            document.write(`<p><i>${week[i]}</i></p>`);
+        } else {
+         document.write(`<p>${week[i]}</p>`);
+       }
+     }
+     
+console.log(week);
 
 
+/* let arr = ['127', '334', '322', '761', '993', '978', '333'];
+
+arr.forEach((e) => {
+       if (e[0] == '3' || e[0] == '7') {
+         document.write(`<br>${e}`);
+       }
+     });
+ */
